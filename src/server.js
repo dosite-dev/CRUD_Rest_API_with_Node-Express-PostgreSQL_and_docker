972d38b8827836eb26010e132e759ae1ce823316
dosite-dev <dositecyiza@gmail.com>
2025-05-04 21:21:58 +0200

    Express server

--- src/server.js
+++ src/server.js
@@ -8,3 +8,13 @@ const app = express()
 const port = process.env.PORT || 3001
 
 // middlewares
+app.use(express.json())
+app.use(cors)
+
+// routes
+// error handling and middlewares
+//server running 
+app.listen(port,() =>{
+    console.log(`Server is running on http:localhost${port}`);
+    
+})
